9e940f9270be3621931a75162fb6e8411824ed04
Alex Lee <x.lee@mail.utoronto.ca>
2017-07-05 20:54:23 -0700

    Can pick between LoremPixel or Unsplash as the image source

--- Pods/Target Support Files/Pods-Quote Pro/Pods-Quote Pro-frameworks.sh
+++ Pods/Target Support Files/Pods-Quote Pro/Pods-Quote Pro-frameworks.sh
@@ -92,11 +92,13 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Nuke/Nuke.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Realm/Realm.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RealmSwift/RealmSwift.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/TBDropdownMenu/TBDropdownMenu.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Nuke/Nuke.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Realm/Realm.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RealmSwift/RealmSwift.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/TBDropdownMenu/TBDropdownMenu.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
